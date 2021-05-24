74c776c6234c6f35ce1fafeb3ab7d1dbe1f39d6c
Javier Rodriguez <javier.rodriguez@geor.pe>
2021-05-23 21:41:44 -0400

    detalles en dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -10,6 +10,21 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var urls = [
+      "../../../assets/images/bg.jpg",
+      "../../../assets/images/bg2.jpeg",
+      "../../../assets/images/bg3.jpeg",
+      "../../../assets/images/bg4.jpeg",
+      "../../../assets/images/bg5.jpeg",
+      ];
+    let site = document.getElementById('site')
+    var count = 1;
+    site.style.background = 'url("' + urls[0] + '")'
+    setInterval(function() {
+      site.style.background = 'url("' + urls[count] + '")'
+      count === urls.length - 1 ? (count = 0) : count++;
+    }, 10000);
+
   }
 
 }
